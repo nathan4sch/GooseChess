a986da99a88eb7476ac0d6304deab35b3770ebf2
Evan <47926489+Evanz142@users.noreply.github.com>
2023-01-22 04:37:14 -0500

    Updates

---
+++ precache-manifest.f1876129384ab58844753e73ff9a5462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a747ff8bd2f6eb0a998fed770b3ef2d7",
+    "url": "/goose-chess/index.html"
+  },
+  {
+    "revision": "0f0a5d1428b5028d6e4c",
+    "url": "/goose-chess/static/css/main.9002f75f.chunk.css"
+  },
+  {
+    "revision": "643a8716deace181ce6f",
+    "url": "/goose-chess/static/js/2.c56d9768.chunk.js"
+  },
+  {
+    "revision": "a3de791e23e29e798b47d2c7ff75799d",
+    "url": "/goose-chess/static/js/2.c56d9768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f0a5d1428b5028d6e4c",
+    "url": "/goose-chess/static/js/main.1c5d5f6b.chunk.js"
+  },
+  {
+    "revision": "24851056647dcbcf2435",
+    "url": "/goose-chess/static/js/runtime-main.70da113e.js"
+  }
+]);
